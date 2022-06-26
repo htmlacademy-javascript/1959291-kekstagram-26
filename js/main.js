e5f591aa376d64b972131f8b530da9c56a995c30
Rutia08 <rutia08@gmail.com>
2022-06-26 13:31:42 +0300

    возвращает состояние для задание 7-1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-import './gallery.js';
+import {createData} from './data.js';
+import {createThumbnails} from './thumbnails.js';
+
+// генерируем данные
+const inputData = createData();
+
+// добавляем данные на страницу
+createThumbnails(inputData);
