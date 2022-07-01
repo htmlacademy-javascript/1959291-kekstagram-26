31ab065a33f8648d265a6b17665e000d3292ac18
Rutia08 <rutia08@gmail.com>
2022-07-01 08:19:01 +0300

    нейминг comments.js

--- js/comments.js
+++ js/comments.js
@@ -4,27 +4,27 @@ import { clearContainer } from './util.js';
 const socialCommentTemplate = document.querySelector('#comment')
   .content
   .querySelector('.social__comment');
-const bigPictureSocial = document.querySelector('.big-picture__social');
-const socialCaption = bigPictureSocial.querySelector('.social__caption');
-const likesCount = bigPictureSocial.querySelector('.likes-count');
-const socialCommentCount = bigPictureSocial.querySelector('.social__comment-count');
-const commentCount = socialCommentCount.querySelector('.comments-count');
-const socialComments = bigPictureSocial.querySelector('.social__comments');
-const commentsLoader = bigPictureSocial.querySelector('.comments-loader');
+const bigPictureSocialContainer = document.querySelector('.big-picture__social');
+const socialCaptionElement = bigPictureSocialContainer.querySelector('.social__caption');
+const likesCountElement = bigPictureSocialContainer.querySelector('.likes-count');
+const socialCommentCountElement = bigPictureSocialContainer.querySelector('.social__comment-count');
+const commentCount = socialCommentCountElement.querySelector('.comments-count');
+const socialCommentsList = bigPictureSocialContainer.querySelector('.social__comments');
+const commentsLoaderElement = bigPictureSocialContainer.querySelector('.comments-loader');
 
 // функция генерации комментариев из данных
 const renderComments = (dataObject) => {
   // заполняем данные social__caption
-  socialCaption.textContent = dataObject.description;
+  socialCaptionElement.textContent = dataObject.description;
 
   // заполняем данные likes-count
-  likesCount.textContent = dataObject.likes;
+  likesCountElement.textContent = dataObject.likes;
 
   // заполняем данные comments-count
   commentCount.textContent = dataObject.comments.length;
 
   // очищаем содержимое socialComments
-  clearContainer(socialComments);
+  clearContainer(socialCommentsList);
 
   // создаем фрагмент для наполнения
   const socialCommentFragment = document.createDocumentFragment();
@@ -38,11 +38,11 @@ const renderComments = (dataObject) => {
   });
 
   // добавляем фрагмент в блок
-  socialComments.appendChild(socialCommentFragment);
+  socialCommentsList.appendChild(socialCommentFragment);
 
   // скрываем ненужные элементы
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+  socialCommentCountElement.classList.add('hidden');
+  commentsLoaderElement.classList.add('hidden');
 };
 
 export { renderComments };
