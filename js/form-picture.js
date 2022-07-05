cb118e4c67618764066563e9699093ac716963c8
Rutia08 <rutia08@gmail.com>
2022-07-05 18:04:34 +0300

    исправление ошибок

--- js/form-picture.js
+++ js/form-picture.js
@@ -13,6 +13,30 @@ const DEFAULT_SCALE_VALUE = 100;
 const SCALE_VALUE_STEP = 25;
 let scaleValue = DEFAULT_SCALE_VALUE;
 
+// ************************ часть 2.2 Наложение эффекта на изображение ***********************************************
+const effectLevelValueElement = imgUploadForm.querySelector('.effect-level__value');
+const effectsListElement = imgUploadForm.querySelector('.effects__list');
+const imgUploadEffectLevelElement = imgUploadForm.querySelector('.img-upload__effect-level');
+const effectLevelSliderElement = imgUploadForm.querySelector('.effect-level__slider');
+
+// состояния слайдера для всех эффектов
+const SLIDER_EFFECT_PARAMETERS = {
+  'chrome': {range: {min: 0, max: 1}, start: 1, step: 0.1}, /* filter: grayscale(0..1) с шагом 0.1 */
+  'sepia': {range: {min: 0, max: 1}, start: 1, step: 0.1}, /* filter: sepia(0..1) с шагом 0.1 */
+  'marvin' : {range: {min: 0, max: 100}, start: 100, step: 1}, /* filter: invert(0..100%) с шагом 1% */
+  'phobos': {range: {min: 0, max: 3}, start: 3, step: 0.1}, /* filter: blur(0..3px) с шагом 0.1px */
+  'heat': {range: {min: 1, max: 3}, start: 3, step: 0.1}, /* filter: brightness(1..3) с шагом 0.1 */
+};
+
+// типы фильтров для эффектов
+const FILTER_EFFECT_TYPES = {
+  'chrome': 'grayscale', /* filter: grayscale(0..1) */
+  'sepia': 'sepia', /* filter: sepia(0..1)*/
+  'marvin': 'invert', /* filter: invert(0..100%)*/
+  'phobos': 'blur',  /* filter: blur(0..3px)*/
+  'heat': 'brightness', /* filter: brightness(1..3) */
+};
+
 // функция изменения свойств элементов
 const changeScale = () => {
   imgUploadPreviewElement.style.transform = `scale(${scaleValue/100})`;
@@ -43,13 +67,6 @@ const addImgUploadPreviewScale = () => {
   scaleControlSmallerElement.addEventListener('click', decreaseScaleValue);
   scaleControlBiggerElement.addEventListener('click', increaseScaleValue);
 };
-
-// ************************ часть 2.2 Наложение эффекта на изображение ***********************************************
-
-const effectLevelValueElement = imgUploadForm.querySelector('.effect-level__value');
-const effectsListElement = imgUploadForm.querySelector('.effects__list');
-const imgUploadEffectLevelElement = imgUploadForm.querySelector('.img-upload__effect-level');
-const effectLevelSliderElement = imgUploadForm.querySelector('.effect-level__slider');
 
 const hideSlider = () => {
   imgUploadEffectLevelElement.classList.add('hidden');
@@ -85,37 +102,20 @@ noUiSlider.create(effectLevelSliderElement, {
   },
 });
 
-// состояния слайдера для всех эффектов
-const sliderEffectParameters = {
-  'chrome': {range: {min: 0, max: 1}, start: 1, step: 0.1}, /* filter: grayscale(0..1) с шагом 0.1 */
-  'sepia': {range: {min: 0, max: 1}, start: 1, step: 0.1}, /* filter: sepia(0..1) с шагом 0.1 */
-  'marvin' : {range: {min: 0, max: 100}, start: 100, step: 1}, /* filter: invert(0..100%) с шагом 1% */
-  'phobos': {range: {min: 0, max: 3}, start: 3, step: 0.1}, /* filter: blur(0..3px) с шагом 0.1px */
-  'heat': {range: {min: 1, max: 3}, start: 3, step: 0.1}, /* filter: brightness(1..3) с шагом 0.1 */
-};
-
-const filterEffectTypes = {
-  'chrome': 'grayscale', /* filter: grayscale(0..1) */
-  'sepia': 'sepia', /* filter: sepia(0..1)*/
-  'marvin': 'invert', /* filter: invert(0..100%)*/
-  'phobos': 'blur',  /* filter: blur(0..3px)*/
-  'heat': 'brightness', /* filter: brightness(1..3) */
-};
-
 // функция добавления фильтра и его значения для превью фото
 const addEffectPreviewFilter = (filter, filterValue) => {
   if (filter === 'phobos') {
-    imgUploadPreviewElement.style.filter = `${filterEffectTypes[filter]}(${filterValue}px)`;
+    imgUploadPreviewElement.style.filter = `${FILTER_EFFECT_TYPES[filter]}(${filterValue}px)`;
     // записываем значение слайдера в поле ввода
     effectLevelValueElement.value = `${filterValue}px`;
     return;
   }
   if (filter === 'marvin') {
-    imgUploadPreviewElement.style.filter = `${filterEffectTypes[filter]}(${filterValue}%)`;
+    imgUploadPreviewElement.style.filter = `${FILTER_EFFECT_TYPES[filter]}(${filterValue}%)`;
     effectLevelValueElement.value = `${filterValue}%`;
     return;
   }
-  imgUploadPreviewElement.style.filter = `${filterEffectTypes[filter]}(${filterValue})`;
+  imgUploadPreviewElement.style.filter = `${FILTER_EFFECT_TYPES[filter]}(${filterValue})`;
   effectLevelValueElement.value = `${filterValue}`;
 };
 
@@ -130,7 +130,7 @@ const addSlider = (data) => {
     hideSlider();
   } else {
     // обновляем параметры шкалы слайдера
-    effectLevelSliderElement.noUiSlider.updateOptions(sliderEffectParameters[`${data.value}`]);
+    effectLevelSliderElement.noUiSlider.updateOptions(SLIDER_EFFECT_PARAMETERS[`${data.value}`]);
 
     // показываем слайдер
     showSlider();
